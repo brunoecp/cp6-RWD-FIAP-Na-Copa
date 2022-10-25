673ba6096304e590b777756e833d3a035a04b5c7
brunoecp <rm95315@fiap.com.br>
2022-10-25 10:38:03 -0300

    nomes inseridos no footer

--- VSCode/fiap-na-copa/src/components/Footer.jsx
+++ VSCode/fiap-na-copa/src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return( 
   <div>
     <p>nome:Bruno Eduardo Caputo Paulino rm:95315</p>
-    <p>nome: rm:</p>
-    <p>nome: rm:</p>
-    <p>nome: rm:</p>
+    <p>nome:Luis Fernando Nascimento de Oliveira rm:93205</p>
+    <p>nome:Felipe Bueno Freitas de Souza rm:94667</p>
+    <p>nome:Guilherme Menezes da Silva rm:89384</p>
     <p>nome:Luisa Gabriele da Purificação rm:95324</p>
   </div> 
     );
